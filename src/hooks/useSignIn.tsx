76b5262763c9222c2dc3f6bda13740fa9bce2393
charleslukes <charleslukes28@gmail.com>
2021-09-09 20:14:25 +0100

    add polkadot api

--- src/hooks/useSignIn.tsx
+++ src/hooks/useSignIn.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { decodeAddress } from '@polkadot/util-crypto';
 import { u8aToHex } from '@polkadot/util';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import pTypes from 'utils/p_types.json';
 
 type Props = {
   account?: any;
@@ -45,11 +47,26 @@ export default function useSignIn() {
       const { web3Accounts, web3FromAddress } = await import(
         '@polkadot/extension-dapp'
       );
+
       const allAccounts = await web3Accounts();
       const currAccount = allAccounts[0];
       const injector = await web3FromAddress(currAccount.address);
       const publicKey = decodeAddress(currAccount.address);
       const hexPublicKey = u8aToHex(publicKey);
+
+      /**
+       * This did not work we need the pallet types for migration and I dont have that knowledge yet
+       * 
+       * Things to do 
+       * 1. Get the balance
+       * 2. Listen for events from the relayer
+       */
+      const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+      // I need know the pallet types for migration the one used is for polkadotIDO 
+      const api = await ApiPromise.create({ provider: wsProvider, types: pTypes });
+      const { nonce, data: balance } = await api.query.system.account(currAccount.address);
+      console.log({ nonce: nonce.toHuman(), data: balance.toJSON() });
+      
       return { currAccount, injector, hexPublicKey };
     } catch (err) {
       setError({ status: true, message: err.message });
