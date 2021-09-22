5c0d4e6632b5b7c7e03ee1d29d3677144e247a01
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2021-09-22 11:24:43 -0300

    Button dots animation

--- src/components/Migration/styles.ts
+++ src/components/Migration/styles.ts
@@ -234,6 +234,29 @@ export const MigrationActions = styled.div`
       margin-bottom: 2rem;
       font-weight: 550;
       cursor: pointer;
+      /* :after {
+        content: '.';
+        animation: dots 1.5s steps(5, end) infinite;
+      }
+
+      @keyframes dots {
+        0%,
+        20% {
+          color: rgba(0, 0, 0, 0);
+          text-shadow: 0.25em 0 0 rgba(0, 0, 0, 0), 0.5em 0 0 rgba(0, 0, 0, 0);
+        }
+        40% {
+          color: white;
+          text-shadow: 0.25em 0 0 rgba(0, 0, 0, 0), 0.5em 0 0 rgba(0, 0, 0, 0);
+        }
+        60% {
+          text-shadow: 0.25em 0 0 white, 0.5em 0 0 rgba(0, 0, 0, 0);
+        }
+        80%,
+        100% {
+          text-shadow: 0.25em 0 0 white, 0.5em 0 0 white;
+        }
+      } */
 
       :disabled {
         cursor: inherit;
