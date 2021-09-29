431b8efdf5521060f92d3bb3b475b420d63f92f1
charleslukes <charleslukes28@gmail.com>
2021-09-29 14:47:38 +0100

    update link for etherscan

--- src/components/Migration/index.tsx
+++ src/components/Migration/index.tsx
@@ -78,7 +78,7 @@ export const MigrationConvert = () => {
                       balance={selectedPolkadotAccount.balance?.free}
                       address={selectedPolkadotAccount.address}
                       // eslint-disable-next-line @typescript-eslint/no-empty-function
-                      changeAccount={() => {}}
+                      changeAccount={() => { }}
                     />
                   }
                 >
@@ -185,23 +185,23 @@ export const MigrationConvert = () => {
           {isMigrated
             ? 'Migrated'
             : status === MIGRATE_STATUS.APPROVING
-            ? 'Approving'
-            : status === MIGRATE_STATUS.AUTHORIZING
-            ? 'Authorizing'
-            : status === MIGRATE_STATUS.PROCESSING_ON_ETHEREUM
-            ? 'Processing on Ethereum'
-            : status === MIGRATE_STATUS.PROCESSING_ON_RELAYER
-            ? 'Processing on Relayer'
-            : status === MIGRATE_STATUS.FAILED
-            ? 'Failed'
-            : 'Migrate Now'}
+              ? 'Approving'
+              : status === MIGRATE_STATUS.AUTHORIZING
+                ? 'Authorizing'
+                : status === MIGRATE_STATUS.PROCESSING_ON_ETHEREUM
+                  ? 'Processing on Ethereum'
+                  : status === MIGRATE_STATUS.PROCESSING_ON_RELAYER
+                    ? 'Processing on Relayer,it will take 15 blocks of confirmation + 1min'
+                    : status === MIGRATE_STATUS.FAILED
+                      ? 'Failed'
+                      : 'Migrate Now'}
         </button>
         <ul>
           {txs.map((tx) => (
             <li key={tx}>
               <a
                 target="_blank"
-                href={`https://ropsten.etherscan.io/tx/${tx}`}
+                href={`https://${process.env.RANGER_HOST_URL.includes("mainnet") ? "" : "ropsten."}etherscan.io/tx/${tx}`}
                 rel="noreferrer"
               >
                 See at Etherscan
