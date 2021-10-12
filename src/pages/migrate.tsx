fffe9a71c0a07f4f61cf12399567b9363e1c7374
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2021-10-12 12:15:39 -0300

    Remove migration subpages

--- src/pages/migrate.tsx
+++ src/pages/migrate.tsx
@@ -5,17 +5,12 @@ import {
   Newsletter,
   Return,
 } from 'components';
-import { useRouter } from 'next/router';
 import React from 'react';
 import * as S from 'styles/home';
 import { HomeTranslations, IHomeTranslations } from 'translations';
 
 export default function Migrate() {
   const { footer, newsletter }: IHomeTranslations = HomeTranslations['en-US'];
-  const router = useRouter();
-  const { id } = router.query;
-
-  if (id !== 'mainnet') router.push('/migrate');
 
   return (
     <S.Wrapper>
