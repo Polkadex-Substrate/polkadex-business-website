c1289816485b7760fac6b8e4c5336389607c4edf
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-04-12 13:31:47 -0300

    update: pixel script

--- src/components/CrowdloansContribute/Template/index.tsx
+++ src/components/CrowdloansContribute/Template/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-danger */
 import { Footer } from 'components';
 import {
   About,
@@ -49,7 +50,6 @@ export const Template = () => {
           src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_CTA}`}
         />
         <script
-          // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{
             __html: `
                 window.dataLayer = window.dataLayer || [];
@@ -58,6 +58,32 @@ export const Template = () => {
                 gtag('config','${process.env.GOOGLE_ANALYTICS_CTA}');
           `,
           }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(e,i){if(!e.pixie){var n=e.pixie=function(e,i,a){n.actionQueue.push({action:e,actionValue:i,params:a})};n.actionQueue=[];var a=i.createElement("script");a.async=!0,a.src="//acdn.adnxs.com/dmp/up/pixie.js";var t=i.getElementsByTagName("head")[0];t.insertBefore(a,t.firstChild)}}(window,document);
+pixie('init', '7a7062db-796b-4937-9aad-8d980417f3ff');
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `pixie('event', 'PageView');`,
+          }}
+        />
+        <noscript>
+          <img
+            src="https://ib.adnxs.com/pixie?pi=7a7062db-796b-4937-9aad-8d980417f3ff&e=PageView&script=0"
+            alt="Pixel by Adnxs"
+            width="1"
+            height="1"
+            style={{ display: 'none' }}
+          />
+        </noscript>
+        <script
+          src="https://secure.adnxs.com/px?id=1577865&t=1"
+          type="text/javascript"
         />
       </Head>
       <Header />
