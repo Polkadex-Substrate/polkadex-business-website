5c66278034b6bf7da8b20b16ddb7860eba951910
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2022-08-22 07:32:22 -0300

    update: remove parachain banner from staking page

--- src/components/Staking/Hero/index.tsx
+++ src/components/Staking/Hero/index.tsx
@@ -22,24 +22,6 @@ export const Hero = ({ apy }) => {
           </S.Aside>
         </S.Container>
       </S.Hero>
-      <S.Banner>
-        <S.BannerContent>
-          <img src="/img/wine.svg" alt="" />
-          <div>
-            <h2>We are a part of the Polkadot ecosystem!</h2>
-            <p>
-              Polkadex has won the 16th Polkadot parachain auction! Thank you
-              for your support!
-            </p>
-          </div>
-        </S.BannerContent>
-        <S.BannerContent>
-          <PrimaryButton
-            content="Read more"
-            href="https://polkadex.trade/crowdloans"
-          />
-        </S.BannerContent>
-      </S.Banner>
     </S.Wrapper>
   );
 };
