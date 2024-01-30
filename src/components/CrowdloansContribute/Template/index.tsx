6a0c2692e250d7014a0deea7751ca69e7644e2fb
Rodolfo Ferreira <kas_pintxuki@outlook.com>
2024-01-30 17:51:21 -0300

    update: google analytics

--- src/components/CrowdloansContribute/Template/index.tsx
+++ src/components/CrowdloansContribute/Template/index.tsx
@@ -10,31 +10,26 @@ import { Stats, StatsCard } from 'components/Stats';
 import { Footer, Header } from 'components/v2';
 import Head from 'next/head';
 import Script from 'next/script';
-import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 import * as S from './styles';
 
 export const Template = () => {
   return (
     <>
-      <GoogleAnalytics />
       <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
+      <Script
+        strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-            !function(e,i){if(!e.pixie){var n=e.pixie=function(e,i,a){n.actionQueue.push({action:e,actionValue:i,params:a})};n.actionQueue=[];var a=i.createElement("script");a.async=!0,a.src="//acdn.adnxs.com/dmp/up/pixie.js";var t=i.getElementsByTagName("head")[0];t.insertBefore(a,t.firstChild)}}(window,document);
-pixie('init', '7a7062db-796b-4937-9aad-8d980417f3ff');
-            `,
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config','${process.env.GOOGLE_ANALYTICS}');
+          `,
         }}
-      />
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `pixie('event', 'PageView');`,
-        }}
-      />
-      <Script
-        src="https://secure.adnxs.com/px?id=1577865&t=1"
-        type="text/javascript"
-        strategy="afterInteractive"
       />
       <S.Wrapper>
         <Head>
