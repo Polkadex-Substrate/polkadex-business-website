81d3c19fb2341734ccfb57da720d5dee1cd9341c
polkadexaj <ajeesh@polkadex.ee>
2026-07-16 01:02:32 -0700

    updated partner section

--- src/components/Home/Partners/index.tsx
+++ src/components/Home/Partners/index.tsx
@@ -1,40 +1,16 @@
+import { SectionHead } from 'components';
 import * as Icons from 'components/Icons';
 
 import { data } from './data';
 import * as S from './styles';
 
-export const Partners = () => {
-  return (
-    <S.Wrapper id="partners">
-      <h2>Partners</h2>
-      <S.Content>
-        <S.ContentFlex>
-          {data[0].map((value, i) => (
-            <Card key={i} {...value} i={i + 1} />
-          ))}
-        </S.ContentFlex>
-        <S.ContentFlex>
-          {data[1].map((value, i) => (
-            <Card key={i} {...value} i={i + 1} />
-          ))}
-        </S.ContentFlex>
-        <S.ContentFlex>
-          {data[2].map((value, i) => (
-            <Card key={i} {...value} i={i + 1} />
-          ))}
-        </S.ContentFlex>
-      </S.Content>
-    </S.Wrapper>
-  );
-};
+type LogoEntry = { icon?: string; img?: string; alt?: string };
 
-type Props = { icon?: string; i: number; alt?: string; img?: string };
-
-const Card = ({ icon = null, i, alt, img = null }: Props) => {
-  const IconComponent = Icons[icon];
+const Card = ({ icon, img, alt }: LogoEntry) => {
+  const IconComponent = icon ? (Icons as Record<string, React.FC>)[icon] : null;
   return (
     <S.Card>
-      {icon?.length ? (
+      {IconComponent ? (
         <IconComponent />
       ) : (
         <img src={`/img/partners/${img}.png`} alt={alt} />
@@ -42,3 +18,27 @@ const Card = ({ icon = null, i, alt, img = null }: Props) => {
     </S.Card>
   );
 };
+
+export const Partners = () => (
+  <S.Wrapper id="partners">
+    <SectionHead
+      eyebrow="Partners"
+      title={
+        <>
+          Building with the <strong>best in Web3</strong>
+        </>
+      }
+      description="Parachains, exchanges, market makers, security firms, and infrastructure providers working alongside Polkadex."
+    />
+
+    <S.Content>
+      {data.map((row, ri) => (
+        <S.Row key={ri}>
+          {row.map((entry, i) => (
+            <Card key={i} {...entry} />
+          ))}
+        </S.Row>
+      ))}
+    </S.Content>
+  </S.Wrapper>
+);
